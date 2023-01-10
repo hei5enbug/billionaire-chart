ec68d40a1828aaec332d541ff7fae26ccbe1e5a6
hei5enbug <kbm0598@gmail.com>
2023-01-10 23:59:45 +0900

    Add Select in AppBar

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,6 +1,14 @@
+import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import { useState } from 'react';
 import FlexBox from './FlexBox';
 
 export default function AppBar() {
+  const [age, setAge] = useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+
   return (
     <FlexBox
       sx={{
@@ -9,7 +17,19 @@ export default function AppBar() {
         marginBottom: 1,
       }}
     >
-      AppBar
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          defaultValue="10"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>BTCUSDT</MenuItem>
+          <MenuItem value={20}>ETHUSDT</MenuItem>
+        </Select>
+      </FormControl>
     </FlexBox>
   );
 }
